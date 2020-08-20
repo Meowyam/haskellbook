7a8f64c093261bf84c0efc0c57c517eaf3089a52
Meowyam <maryam@chilon.net>
2020-08-20 09:58:32 +0800

    tyger tyger

--- 9.hs
+++ 9.hs
@@ -1,3 +1,12 @@
+module PoemLines where
+firstSen = "Tyger Tyger, burning bright\n"
+secondSen = "In the forests of the night\n"
+thirdSen = "What immortal hand or eye\n"
+fourthSen = "Could frame thy fearful\
+\ symmetry?"
+sentences = firstSen ++ secondSen ++ thirdSen ++ fourthSen
+
+
 safeTail :: [a] -> Maybe [a]
 safeTail [] = Nothing
 safeTail (_:[]) = Nothing
@@ -20,7 +29,32 @@ eftChar = eftGen
 
 -- so after writing out each one individually, I realised that everything was essentially this:
 -- so here is a generalised eft definition
-eftGen x y
-  | x > y = []
-  | x == y = [x]
-  | otherwise = x : (eftGen (succ x) y) 
+eftGen xs y
+  | xs > y = []
+  | xs == y = [xs]
+  | otherwise = xs : (eftGen (succ xs) y) 
+
+-- thy fearful symmetry
+--
+myWords [] = []
+myWords (' ':xs) = myWords xs
+myWords xs = (takeWhile (/=' ') xs) : (myWords (dropWhile (/=' ') xs))
+
+shouldEqual = [ "Tyger Tyger, burning bright", "In the forests of the night", "What immortal hand or eye", "Could frame thy fearful symmetry?"]
+
+myLines :: String -> [String]
+myLines [] = []
+myLines ('\n':xs) = myLines xs
+myLines xs = (takeWhile (/='\n') xs) : (myLines (dropWhile (/='\n') xs))
+
+main :: IO ()
+main =
+  print $
+  "Are they equal? "
+  ++ show (myLines sentences
+            == shouldEqual)
+
+myGeneric s [] = []
+myGeneric s (x:xs)
+  | s == x = myGeneric s xs
+  | otherwise = (takeWhile(/=s) (x:xs)) : (myGeneric s (dropWhile (/=s) (x:xs)))
