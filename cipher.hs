4b6f143dea2a1b0d33f5cceaacd9ea10db8201c5
Meowyam <maryam@chilon.net>
2020-08-21 11:59:41 +0800

    added unCaesar to cipher

--- cipher.hs
+++ cipher.hs
@@ -3,8 +3,12 @@ import Data.Char
 
 alphabet = ['a'..'z']
 countChar c = length $ takeWhile(/=c) alphabet 
-caesar 0 (x:xs) = x:xs
+caesar 0 x = x
 caesar n [] = []
-caesar n (x:xs) = (alphabet !! (3 + (countChar x))) : caesar n xs
+caesar n (x:xs) = (alphabet !! (n + (countChar x))) : caesar n xs
 
-
+unCaesar 0 x = x
+unCaesar n [] = []
+unCaesar n (x:xs)
+  | (countChar x) < n = (alphabet !! ((countChar x) + (length alphabet) - n)) : unCaesar n xs
+  | otherwise = (alphabet!!((countChar x)-n)) : unCaesar n xs
