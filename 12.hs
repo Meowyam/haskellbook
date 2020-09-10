59e5892b01067833fa8943d985e0f4a9b9baa3fd
Meowyam <maryam@chilon.net>
2020-09-10 16:38:58 +0800

    it's only natural

--- 12.hs
+++ 12.hs
@@ -45,3 +45,43 @@ countVowels phrase = go xs
     go (x:xs)
       | checkVowel x = 1 + go xs
       | otherwise = go xs
+
+-- validate the word
+
+newtype Word' =
+  Word' String
+  deriving (Eq, Show)
+
+vowels = "aeiou"
+
+-- if vowels > not vowels, return Nothing
+
+checkCons :: String -> Integer
+checkCons " " = 0
+checkCons phrase = (fromIntegral $ length phrase) - countVowels phrase
+
+mkWord :: String -> Maybe Word'
+mkWord str
+  | countVowels str > checkCons str = Nothing
+  | otherwise = Just (Word' str)
+
+-- it's only natural
+-- naturals :: whole numbers from 0 to infinity
+--  convert natural numbers to integers and integers to naturals
+--  integer incl negativ, nat doesn't
+
+data Nat =
+  Zero
+    | Succ Nat
+    deriving (Eq, Show)
+
+natToInteger :: Nat -> Integer
+natToInteger Zero = 0
+natToInteger (Succ n) = 1 + natToInteger n
+
+integerToNat :: Integer -> Maybe Nat
+integerToNat n
+  | n < 0 = Nothing
+  | n == 0 = Just Zero
+  | otherwise = case integerToNat (n-1) of
+                  Just n -> Just (Succ n)
