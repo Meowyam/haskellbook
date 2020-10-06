0fb46b52e66fe4195a3e9900b07a82be331657d4
Meowyam <maryam@chilon.net>
2020-10-06 19:42:09 +0800

    working test, rewrite cipher

--- cipher/src/Lib.hs
+++ cipher/src/Lib.hs
@@ -2,65 +2,30 @@ module Lib where
 import Data.Char
 import Data.List ()
 
-alphabet :: [Char]
-alphabet = ['a'..'z']
-countChar :: Char -> Int
-countChar c = length $ takeWhile(/=c) alphabet 
-
 --caesar
 
+shiftChar :: Int -> Char -> Char
+shiftChar n x = chr (mod (ord (toLower x) - ord 'a' + n) 26 + ord 'a')
+
+vShift :: Char -> Int
+vShift k
+  | isLetter k = ord (toLower k) - ord 'a'
+  | otherwise = 0
+
 caesar :: Int -> String -> String
-caesar 0 x = x
-caesar n [] = []
-caesar n (x:xs) = (alphabet !! (n + (countChar x))) : caesar n xs
-  where x = toLower x
+caesar n = map (shiftChar n)
 
 unCaesar :: Int -> String -> String
-unCaesar 0 x = x
-unCaesar n [] = []
-unCaesar n (x:xs)
-  | (countChar x) < n = (alphabet !! ((countChar x) + (length alphabet) - n)) : unCaesar n xs
-  | otherwise = (alphabet!!((countChar x)-n)) : unCaesar n xs
-    where x = toLower x
-
+unCaesar n = caesar (-n)
+  
 --vigenere
 
 --keyword = "ally"
 --message = "meet at dawn"
-
-getLetterval :: String -> [Int]
-getLetterval = map countChar
-
-keyVal :: String -> [Int]
-keyVal k = getLetterval k
-
-getMsgval :: String -> [Int]
-getMsgval = getLetterval . concat . words
-
--- drop the last index otherwise it adds another list at the end
-whereSpaces :: String -> [Int]
-whereSpaces = init . scanl1 (+) . (map length . words)
-
--- I used the cycle
-vCryptval :: String -> String -> [Int]
-vCryptval m k = [if x > (length alphabet) then x-(length alphabet) else x | x <-(zipWith (+) (cycle (keyVal k)) (getMsgval m))]
-
-vCryptchar :: String -> String -> String
-vCryptchar m k = map (alphabet !!) $ vCryptval m k
-
--- split into where spaces should go and UNWORD into a string
-addSpaces :: String -> String -> String
-addSpaces m whichCrypt = unwords $ foldr (\space acc -> [fst $ splitAt space $ head acc] ++ [snd $ splitAt space $ head acc] ++ (tail acc)) [(whichCrypt)] (whereSpaces m) 
-
 doVigenere :: String -> String -> String
-doVigenere m k = addSpaces m (vCryptchar m k)
-
-unCryptval :: String -> String -> [Int]
-unCryptval m k = [if x < 0 then x+(length alphabet) else x | x <-(zipWith (-) (getMsgval m)(cycle (keyVal k))) ]
-unCryptchar :: String -> String -> String
-unCryptchar m k = map (alphabet !!) $ (unCryptval m k)
+doVigenere ks ms = zipWith go (cycle ks) ms
+  where go k m = shiftChar (vShift k) m
 
 unVigenere :: String -> String -> String
-unVigenere m k = addSpaces m (unCryptchar m k)
-
-
+unVigenere ks ms = zipWith go (cycle ks) ms
+  where go k m = shiftChar (-vShift k) m
--- cipher/test/Spec.hs
+++ cipher/test/Spec.hs
@@ -2,13 +2,35 @@ module Main where
 import Lib ( caesar, doVigenere, unCaesar, unVigenere )
 import Test.QuickCheck
 
+randomChar :: Gen Char
+randomChar = elements ['a'..'z']
+
+randomString :: Gen String
+randomString = listOf randomChar
+
+genCaesar :: Gen (Int, String)
+genCaesar = do
+  n <- arbitrary
+  message <- randomString
+  return (n, message)
+
+genVigenere :: Gen (String, String)
+genVigenere = do
+  k <- randomChar
+  ks <- randomString
+  m <- randomChar
+  ms <- randomString
+  return ((k:ks), (m:ms))
+
+testCaesar :: Property
+testCaesar = forAll genCaesar (\(n, message) ->
+  unCaesar n (caesar n message) == message)
+
+testVigenere :: Property
+testVigenere = forAll genVigenere (\(keyword, message) ->
+  unVigenere keyword (doVigenere keyword message) == message)
+
 main :: IO ()
 main = do
   quickCheck testCaesar
   quickCheck testVigenere
-
-testCaesar :: Int -> String -> Bool
-testCaesar n message = ((Lib.unCaesar n (Lib.caesar n message)) == message)
-
-testVigenere :: String -> String -> Bool
-testVigenere keyword message = (Lib.unVigenere keyword (Lib.doVigenere keyword message)) == message
