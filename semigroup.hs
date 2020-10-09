41ac60e8e02794f172cd50794e8757212d7da076
Meowyam <maryam@chilon.net>
2020-10-09 18:30:26 +0800

    semigroup exercises up to boolconj

--- semigroup.hs
+++ semigroup.hs
@@ -34,7 +34,70 @@ instance Semigroup a => Semigroup (Identity a) where
 type IdentAssoc =
   Identity Trivial -> Identity Trivial -> Identity Trivial -> Bool
 
+-- datatwo
+
+data Two a b = Two a b deriving (Eq, Show)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    return (Two a b)
+
+instance (Semigroup a, Semigroup b) => Semigroup (Two a b) where
+  (Two a b) <> (Two x y) = Two (a <> x) (b <> y)
+
+type TwoTriv = Two Trivial Trivial
+
+type TwoAssoc =
+  TwoTriv -> TwoTriv -> TwoTriv -> Bool
+
+-- datathree
+
+data Three a b c = Three a b c deriving (Eq, Show)
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c) => Arbitrary (Three a b c) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    c <- arbitrary
+    return (Three a b c)
+
+instance (Semigroup a, Semigroup b, Semigroup c)
+  => Semigroup (Three a b c) where
+  (Three a b c) <> (Three x y z) = Three (a <> x) (b <> y) (c <> z)
+
+type ThreeTriv = Three Trivial Trivial Trivial
+
+type ThreeAssoc =
+  ThreeTriv -> ThreeTriv -> ThreeTriv -> Bool
+
+--and four is the same
+-- boolconj
+
+newtype BoolConj = BoolConj Bool deriving (Eq, Show)
+
+instance Arbitrary BoolConj where
+  arbitrary = 
+    frequency [ (1, return (BoolConj True))
+              , (1, return (BoolConj False)) ]
+
+instance Semigroup Bool where
+  True <> True = True
+  _ <> False = False
+  False <> _ = False
+
+instance Semigroup BoolConj where
+    BoolConj a <> BoolConj b = BoolConj (a <> b) 
+
+type BoolConjAssoc =
+  BoolConj -> BoolConj -> BoolConj -> Bool
+
+--
 main :: IO ()
 main = do
   quickCheck (semigroupAssoc :: TrivAssoc)
   quickCheck (semigroupAssoc :: IdentAssoc) 
+  quickCheck (semigroupAssoc :: TwoAssoc)
+  quickCheck (semigroupAssoc :: ThreeAssoc)
+  quickCheck (semigroupAssoc :: BoolConjAssoc)
