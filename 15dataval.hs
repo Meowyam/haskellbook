376791483470bbe86e7384ebf9bdc32f6a874e7d
Meowyam <maryam@chilon.net>
2020-10-14 13:51:50 +0800

    add quickcheck to dataval

--- 15dataval.hs
+++ 15dataval.hs
@@ -1,4 +1,13 @@
 import Data.Semigroup
+import Test.QuickCheck hiding (Failure, Success)
+
+data Trivial = Trivial deriving (Eq, Show)
+
+instance Semigroup Trivial where
+  _ <> _ = Trivial 
+
+instance Arbitrary Trivial where
+  arbitrary = return Trivial
 
 data Validation a b =
   Failure a | Success b
@@ -10,15 +19,20 @@ instance Semigroup a =>
    Success a <> _ = Success a 
    _ <> Success b = Success b
 
-main = do
-  let failure :: String
-              -> Validation String Int
-      failure = Failure
-      success :: Int
-              -> Validation String Int
-      success = Success
-  print $ success 1 <> failure "blah"
-  print $ failure "woot" <> failure "blah"
-  print $ success 1 <> success 2
-  print $ failure "woot" <> success 2
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Validation a b) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    frequency [(1, return (Failure a)),
+               (1, return (Success b))]
 
+semigroupAssoc :: (Eq m, Semigroup m) => m -> m -> m -> Bool
+semigroupAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
+
+type ValTriv = Validation Trivial Trivial
+
+type ValAssoc = ValTriv -> ValTriv -> ValTriv -> Bool
+
+main :: IO ()
+main =
+  quickCheck (semigroupAssoc :: ValAssoc)
