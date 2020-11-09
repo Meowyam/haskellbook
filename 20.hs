69b92eac71350fdca36d2194b87e3e241ea03c71
Meowyam <maryam@chilon.net>
2020-11-09 10:09:03 +0800

    add foldable instances

--- 20.hs
+++ 20.hs
@@ -1,6 +1,12 @@
 import Data.Monoid
 import Data.Foldable
 
+import Test.QuickCheck
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes
+
+--
+
 data Identity a =
   Identity a
   deriving (Eq, Show)
@@ -79,3 +85,127 @@ foldMap'' :: (Foldable t, Monoid m)
           => (a -> m) -> t a -> m
 foldMap'' f = foldr (\x -> ((<>) (f x))) mempty
 -- foldMap'' Sum [1,2,3] = 6
+--
+-- write foldable instances
+--
+data Constant a b =
+  Constant b
+  deriving (Eq, Show)
+
+instance Foldable (Constant a) where
+  foldMap f (Constant x) = f x
+
+instance Arbitrary b => Arbitrary (Constant a b) where
+  arbitrary = do
+    b <- arbitrary
+    return $ Constant b
+
+instance (Eq a, Eq b) => EqProp (Constant a b) where
+  (=-=) = eq
+
+--
+
+data Two a b =
+  Two a b
+  deriving (Eq, Show)
+
+instance Foldable (Two a) where
+  foldMap f (Two x y) = f y
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    return $ Two a b
+
+instance (Eq a, Eq b) => EqProp (Two a b) where
+  (=-=) = eq
+
+--
+
+data Three a b c =
+  Three a b c
+  deriving (Eq, Show)
+
+instance Foldable (Three a b) where
+  foldMap f (Three x y z) = f z
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c) => Arbitrary (Three a b c) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    c <- arbitrary
+    return $ Three a b c
+
+instance (Eq a, Eq b, Eq c) => EqProp (Three a b c) where
+  (=-=) = eq
+
+--
+
+data Three' a b =
+  Three' a b b
+  deriving (Eq, Show)
+
+instance Foldable (Three' a) where
+  foldMap f (Three' x y z) = f y
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Three' a b) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    c <- arbitrary
+    return $ Three' a b c
+
+instance (Eq a, Eq b) => EqProp (Three' a b) where
+  (=-=) = eq
+
+--
+
+data Four' a b =
+  Four' a b b b
+  deriving (Eq, Show)
+
+instance Foldable (Four' a) where
+  foldMap f (Four' w x y z) = f x 
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Four' a b) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    c <- arbitrary
+    d <- arbitrary
+    return $ Four' a b c d
+
+instance (Eq a, Eq b) => EqProp (Four' a b) where
+  (=-=) = eq
+
+--
+filterF :: ( Applicative f
+           , Foldable t
+           , Monoid (f a))
+           => (a -> Bool) -> t a -> f a
+filterF f = foldr (\x y ->
+  if f x
+     then (pure x) <> y
+     else y)
+  mempty
+--
+
+type Testblob = (Int, String, [Int], Int, String)
+
+main = do
+  let consttrig :: Constant Int Testblob 
+      consttrig = undefined
+  let twotrig :: Two Int Testblob
+      twotrig = undefined
+  let threetrig :: Three Int Int Testblob
+      threetrig = undefined
+  let thretrig :: Three' Int Testblob
+      thretrig = undefined
+  let fourtrig :: Four' Int Testblob
+      fourtrig = undefined
+  quickBatch $ foldable consttrig
+  quickBatch $ foldable twotrig
+  quickBatch $ foldable threetrig
+  quickBatch $ foldable thretrig
+  quickBatch $ foldable fourtrig
