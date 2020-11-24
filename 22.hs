face9a1471203924c15825de5f288834b7ab4220
Meowyam <maryam@chilon.net>
2020-11-24 14:45:12 +0800

    reader monad

--- 22.hs
+++ 22.hs
@@ -118,3 +118,50 @@ instance Applicative (Reader r) where
         -> Reader r b
   (Reader rab) <*> (Reader ra) =
     Reader $ \r -> rab r $ ra r
+
+--
+
+foo :: (Functor f, Num a) => f a -> f a
+foo r = fmap (+1) r
+
+bar :: Foldable f => t -> f a -> (t, Int)
+bar r t = (r, length t)
+
+froot :: Num a => [a] -> ([a], Int)
+froot r = (map (+1) r, length r)
+
+--barOne :: Foldable t => t a -> (t a, Int)
+barOne :: (Functor t, Num a, Foldable t) => t a -> (t a, Int)
+barOne r = (foo r, length r)
+
+frooty :: Num a => [a] -> ([a], Int)
+frooty r = bar (foo r) r
+
+frooty' :: Num a => [a] -> ([a], Int)
+frooty' = \r -> bar (foo r) r
+
+fooBind m k = \r -> k (m r) r
+fooBind :: (r -> a)
+        -> (a -> r -> b)
+        -> (r -> b)
+
+-- exercise: reader monad
+
+instance Monad (Reader r) where
+  return = pure
+
+  (>>=) :: Reader r a
+        -> (a -> Reader r b)
+        -> Reader r b
+  (Reader ra) >>= aRb =
+    Reader $ \r -> runReader (aRb (ra r)) r
+
+--newtype Reader r a =
+  --Reader { runReader :: r -> a }
+
+getDogRM :: Person -> Dog
+getDogRM =
+  dogName >>= \name ->
+  address >>= \ addy ->
+  return $ Dog name addy
+
